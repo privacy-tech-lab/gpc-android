1e86ebd5ca7eac3b3df42a19d940e331852fd8e3
SebastianZimmeck <sebastian@sebastianzimmeck.de>
2019-06-23 15:03:59 -0400

    Adding first iOS app test project (Google Analytics integration)

---
+++ test2/test2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by Zimmeck, Sebastian on 6/23/19.
+//  Copyright © 2019 Zimmeck, Sebastian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
