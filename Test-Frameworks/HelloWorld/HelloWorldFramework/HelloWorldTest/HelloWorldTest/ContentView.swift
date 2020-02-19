f6dcccce13432aa5736a6513854729d939679b3e
Rafael Goldstein <rgoldstein01@wesleyan.edu>
2020-02-19 10:51:57 -0500

    added test framework for #8

---
+++ Test-Frameworks/HelloWorld/HelloWorldFramework/HelloWorldTest/HelloWorldTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorldTest
+//
+//  Created by Rafael Goldstein on 2/19/20.
+//  Copyright © 2020 Rafael Goldstein. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
