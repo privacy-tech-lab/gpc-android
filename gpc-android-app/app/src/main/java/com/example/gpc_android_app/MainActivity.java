fac95d0206b4c422e8e339751cf8d0fd13ff3da3
Nishant <naggarwal@wesleyan.edu>
2023-04-27 13:01:16 -0400

    Added buttons to redirect user to gpc browsers.

--- gpc-android-app/app/src/main/java/com/example/gpc_android_app/MainActivity.java
+++ gpc-android-app/app/src/main/java/com/example/gpc_android_app/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.gpc_android_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.content.ComponentName;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -18,14 +21,22 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button adid_settings_btn;
+    Button brave_browser_btn;
+    Button duckgo_browser_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        adid_settings_btn = findViewById(R.id.button);
+        adid_settings_btn = findViewById(R.id.adid_button);
         adid_settings_btn.setOnClickListener(this);
+
+        brave_browser_btn = findViewById(R.id.brave_btn);
+        brave_browser_btn.setOnClickListener(this);
+
+        duckgo_browser_btn = findViewById(R.id.duckgo_btn);
+        duckgo_browser_btn.setOnClickListener(this);
 
     }
 
@@ -65,11 +76,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        /* Redirects user to their AdId settings.
+         */
+        if (v.getId() == R.id.adid_button) {
+            if (hasAdSettings(this)) {
+                startActivity(adSettings());
+            } else {
+                Toast.makeText(this, "Error", Toast.LENGTH_LONG).show();
+            }
+        }
+        /* Opens the brave app in PlayStore using intents; if PlayStore is not installed,
+         * redirects user to browser google play.
+         */
+        else if (v.getId() == R.id.brave_btn) {
+            try {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("market://details?id=com.brave.browser"));
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            } catch (ActivityNotFoundException e) {
+                Log.e("App-log", "Could not find Market");
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://play.google.com/store/apps/details?id=com.brave.browser"));
+                startActivity(intent);
+            }
 
-        if (hasAdSettings(this)) {
-            startActivity(adSettings());
-        } else {
-            Toast.makeText(this, "Error", Toast.LENGTH_LONG).show();
+        }
+        /* Opens the duck-duck go app in PlayStore using intents; if PlayStore is not installed,
+         * redirects user to browser google play.
+         */
+        else if (v.getId() == R.id.duckgo_btn) {
+            try {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("market://details?id=com.duckduckgo.mobile.android"));
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            } catch (ActivityNotFoundException e) {
+                Log.e("App-log", "Could not find Market");
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://play.google.com/store/apps/details?id=com.duckduckgo.mobile.android"));
+                startActivity(intent);
+            }
         }
 
     }
