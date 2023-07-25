c04fcbcc450e9652d60980466c86745eed4d00da
Konrad Kollnig <5175206+kasnder@users.noreply.github.com>
2023-07-25 21:24:20 +0200

    add conversion script

---
+++ scripts/zhar2har.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# run: source [path to script]/zhar2har.sh
+# assumption: recordings in zhar/
+
+mkdir -p zhar_done/
+mkdir -p har/
+
+for f in zhar/*.zhar; do
+    f="${f%.*}"
+    mv $f.zhar $f.zz
+    pigz -k -d -z $f.zz
+    
+    mv $f $f.json
+    mv $f.json har/
+
+    mv $f.zz $f.zhar
+    mv $f.zhar zhar_done/
+done
+
+# repeat for hidden files
+for f in zhar/.*.zhar; do
+    f="${f%.*}"
+    mv $f.zhar $f.zz
+    pigz -k -d -z $f.zz
+    
+    mv $f $f.json
+    mv $f.json har/
+
+    mv $f.zz $f.zhar
+    mv $f.zhar zhar_done/
+done
+
